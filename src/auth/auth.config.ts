6d43392f14ac8af59d669fc8082842e9ceb2c01c
YCS22 <68285199+YCS22@users.noreply.github.com>
2022-06-15 12:35:22 +0300

    Update auth.config.ts

--- src/auth/auth.config.ts
+++ src/auth/auth.config.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthConfig {
-  public userPoolId = 'us-east-2_XvQOwiZRd';
-  public clientId = '2a9emmpae10jough4vnochotft';
-  public region = 'us-east-2';
-  public authority = `https://cognito-idp.us-east-2.amazonaws.com/us-east-2_XvQOwiZRd`;
+  public userPoolId = '';
+  public clientId = '';
+  public region = '';
+  public authority = ``;
 }
